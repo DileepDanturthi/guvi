fa501458d708ee3b1761163a33137cebe3bd9811
DileepDanturthi <51699286+DileepDanturthi@users.noreply.github.com>
2019-07-16 12:04:51 +0530

    Create median of array.c

---
+++ median of array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+    void main()
+    {
+        int i,j,n,a[100],temp;
+        float median;
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        for(i=0;i<n;++i)
+        {
+            for(j=i+1;j<n;++j)
+            {
+                if (a[i]>a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        for(i=0;i<=n;i++)
+    
+        
+        {
+            if(n%2 == 0)
+            median = (a[(n-1)/2] + a[n/2])/2.0;
+            else
+            median = a[n/2];
+            
+        }
+         printf("%f",median);
+        
+    }
