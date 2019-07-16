cc7592cd747df8a08e9909cd5f6438c4523e1f0b
DileepDanturthi <51699286+DileepDanturthi@users.noreply.github.com>
2019-07-16 10:34:19 +0530

    Update multiples of 5.c

--- multiples of 5.c
+++ multiples of 5.c
@@ -3,9 +3,9 @@ int main()
 {
         int sum=0,i,n;
         scanf("%d",&n);
-        for(i=1;i<=n;i++)
+        for(i=1;i<=5;i++)
         {
-            printf("%d\t",5*i);
+            printf("%d\t",i*n);
         }
         return 0;
 }
